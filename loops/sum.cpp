54b964aa67af68676334810dad5105d0723a3128
JiyanshThakral <jiyanshthakral22@gmail.com>
2026-07-17 17:10:52 +0530

    sum-using-loops

---
+++ loops/sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a=0;
+    int n;
+   cin >> n;
+   for (int i = 1 ; i<=n; i++)
+   a=a+i;
+
+   cout << "The sum of the numbers from 1 to " << n << " is " << a << endl;
+
+}
