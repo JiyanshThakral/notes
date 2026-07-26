8f5ccca35c2cdadf28162241b6aedfcf9317c165
JiyanshThakral <jiyanshthakral22@gmail.com>
2026-07-26 19:36:35 +0530

    patterns

---
+++ Nested Loops (patterns)/squarePatternChars.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int n;
+    cout << "enter the number of rows : ";
+    cin >> n;
+    
+    for (int i=1; i<=n; i++) {
+        for (char j='A'; j<'A'+n; j++) {
+            cout << j << " ";
+        }
+
+        cout << endl;
+    }
+}
